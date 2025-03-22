889ce02f4b478df5aa4f12a7efa80d25b21c6b5a
MartinFerret <mferret.pro@gmail.com>
2025-03-22 22:43:06 +0100

    feat: UI library.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {provideRouter, withComponentInputBinding} from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import {provideNoopAnimations} from '@angular/platform-browser/animations';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withComponentInputBinding()),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+    provideNoopAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
+  ]
 };
