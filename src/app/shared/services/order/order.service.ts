ba1ef1cca91c138d23912f98e605561bafd5fdae
MartinFerret <mferret.pro@gmail.com>
2025-03-22 22:50:16 +0100

    feat: service api.

---
+++ src/app/shared/services/order/order.service.ts
@@ -0,0 +1,17 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Order} from '../../models/order.model';
+import {environment} from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+ private readonly httpClient = inject(HttpClient);
+
+ public getOrders(): Observable<Order[]> {
+   return this.httpClient.get<Order[]>(`${environment.apiURL}/orders`);
+  }
+}
